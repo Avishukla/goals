81245bfaefd1f5fbcec0f3498cef84f708bc56eb
Avanishsh <avishukl96@gmail.com>
2020-01-07 01:00:27 +0530

    welcoe page title changes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('welcome/', function () {
+    return view('welcome');
+});
+
 Route::get('home/', function () {
     return view('template');
 });
